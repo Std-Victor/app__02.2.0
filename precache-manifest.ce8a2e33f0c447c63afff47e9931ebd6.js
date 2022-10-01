0b651868002332e7d48420e44b4d80156ddca1cb
Std-Victor <smallvictor220@gmail.com>
2022-10-01 18:20:33 +0100

    Updates

---
+++ precache-manifest.ce8a2e33f0c447c63afff47e9931ebd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2f25d6f5f52628124dc",
+    "url": "/app__02/static/css/main.0c2b91b0.chunk.css"
+  },
+  {
+    "revision": "c2f25d6f5f52628124dc",
+    "url": "/app__02/static/js/main.15d963d4.chunk.js"
+  },
+  {
+    "revision": "532fc142c0d2b4fe8bdf",
+    "url": "/app__02/static/js/runtime~main.c9932a13.js"
+  },
+  {
+    "revision": "83f06311eacd5cb0f0cd",
+    "url": "/app__02/static/js/2.05258c36.chunk.js"
+  },
+  {
+    "revision": "e2aa523fcf964839130f",
+    "url": "/app__02/static/js/3.ec459d4c.chunk.js"
+  },
+  {
+    "revision": "c780a060d24a070379eef40299de4031",
+    "url": "/app__02/index.html"
+  }
+];
